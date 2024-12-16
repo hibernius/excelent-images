da740ce68906ec30776fc21f5fb572b31e98c2d5
tkc <korova.cow@gmail.com>
2024-12-16 01:41:52 +0000

    Implement CLI args

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
+use clap::{Arg, Command};
 use image::io::Reader as ImageReader;
 use image::{DynamicImage, GenericImageView, ImageError};
 use rust_xlsxwriter::{ColNum, Format, RowNum, Workbook};
+use std::path::{Path, PathBuf};
 
 fn main() {
-    let image = open_image("image-name.ext");
-    let mut workbook = Workbook::new();
 
-    let image = match image {
+    let opts = get_opts();
+
+    let input_filepath = opts.get_one::<String>("input_file")
+        .expect("Error: Input file argument is missing. Please provide a valid file path.");
+    let output_directory = opts.get_one::<String>("output_directory")
+        .expect("Error: Output argument is missing. Please provide a valid output directory name.");
+
+    let image = match get_image(input_filepath) {
         Ok(img) => img,
         Err(error) => {
             eprintln!("Error opening or decoding image: {:?}", error);
@@ -15,6 +22,7 @@ fn main() {
     };
 
     let (width, height) = get_image_dimensions(image.clone());
+    let mut workbook = Workbook::new();
     let worksheet = workbook.add_worksheet();
 
     for col in 0..width {
@@ -27,14 +35,63 @@ fn main() {
         }
     }
 
-    workbook.save("output.xlsx").expect("Error saving workbook.");
+    let filename = get_filename(input_filepath).unwrap_or_else(|| "unknown".to_string());
+    let filename = replace_extension(&filename, "xlsx");
+    let output_directory = enforce_trailing_slash(output_directory);
+
+    workbook
+        .save(format!("{}{}", output_directory, filename))
+        .expect("Error saving workbook.");
 }
 
-fn open_image(path: &str) -> Result<DynamicImage, ImageError> {
-    return ImageReader::open(path)?.decode();
+fn get_opts() -> clap::ArgMatches {
+    return Command::new("ExcelentImages")
+        .about("Converts an image file to an Excel file.")
+        .arg(Arg::new("input_file")
+            .short('f')
+            .long("file")
+            .value_name("INPUT_FILE")
+            .help("Path to the image file")
+            .value_parser(clap::value_parser!(String))
+            .required(true)
+        )
+        .arg(Arg::new("output_directory")
+            .short('d')
+            .long("dir")
+            .value_name("OUTPUT_DIR")
+            .default_value("output/")
+            .help("Output directory path (include trailing slash)")
+            .value_parser(clap::value_parser!(String))
+            .required(false)
+        )
+        .get_matches();
+}
+
+fn get_image(path: &str) -> Result<DynamicImage, ImageError> {
+    let reader = ImageReader::open(path)?;
+    let image = reader.decode()?;
+    Ok(image)
 }
 
 fn get_image_dimensions(image: DynamicImage) -> (u32, u32) {
     return image.dimensions();
 }
 
+fn get_filename(path: &str) -> Option<String> {
+    let path = Path::new(path);
+    path.file_stem().map(|os_str| os_str.to_string_lossy().to_string())
+}
+
+fn replace_extension(path: &str, new_extension: &str) -> String {
+    let mut path_buf = PathBuf::from(path);
+    path_buf.set_extension(new_extension);
+    path_buf.to_string_lossy().to_string()
+}
+
+fn enforce_trailing_slash(dir_path: &str) -> String {
+    if dir_path.ends_with('/') {
+        dir_path.to_string()
+    } else {
+        format!("{}/", dir_path)
+    }
+}
