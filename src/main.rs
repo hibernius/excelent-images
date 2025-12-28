6836ac858e169b9223dd4ebcdee4a3b95c5b1660
dave <korova.cow@gmail.com>
2025-12-28 23:04:38 +0000

    Refactor needless_return

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,13 @@ use rust_xlsxwriter::{ColNum, Format, RowNum, Workbook};
 use std::path::{Path, PathBuf};
 
 fn main() {
-
     let opts = get_opts();
 
-    let input_filepath = opts.get_one::<String>("input_file")
+    let input_filepath = opts
+        .get_one::<String>("input_file")
         .expect("Error: Input file argument is missing. Please provide a valid file path.");
-    let output_directory = opts.get_one::<String>("output_directory")
+    let output_directory = opts
+        .get_one::<String>("output_directory")
         .expect("Error: Output argument is missing. Please provide a valid output directory name.");
 
     let image = match get_image(input_filepath) {
@@ -26,7 +27,9 @@ fn main() {
     let worksheet = workbook.add_worksheet();
 
     for col in 0..width {
-        worksheet.set_column_width(col as ColNum, 2).expect("Error setting column width.");
+        worksheet
+            .set_column_width(col as ColNum, 2)
+            .expect("Error setting column width.");
         for row in 0..height {
             let pixel = image.get_pixel(col, row);
             let color = format!("#{:02X}{:02X}{:02X}", pixel[0], pixel[1], pixel[2]);
@@ -45,26 +48,28 @@ fn main() {
 }
 
 fn get_opts() -> clap::ArgMatches {
-    return Command::new("ExcelentImages")
+    Command::new("ExcelentImages")
         .about("Converts an image file to an Excel file.")
-        .arg(Arg::new("input_file")
-            .short('f')
-            .long("file")
-            .value_name("INPUT_FILE")
-            .help("Path to the image file")
-            .value_parser(clap::value_parser!(String))
-            .required(true)
+        .arg(
+            Arg::new("input_file")
+                .short('f')
+                .long("file")
+                .value_name("INPUT_FILE")
+                .help("Path to the image file")
+                .value_parser(clap::value_parser!(String))
+                .required(true),
         )
-        .arg(Arg::new("output_directory")
-            .short('d')
-            .long("dir")
-            .value_name("OUTPUT_DIR")
-            .default_value("output/")
-            .help("Output directory path (include trailing slash)")
-            .value_parser(clap::value_parser!(String))
-            .required(false)
+        .arg(
+            Arg::new("output_directory")
+                .short('d')
+                .long("dir")
+                .value_name("OUTPUT_DIR")
+                .default_value("output/")
+                .help("Output directory path (include trailing slash)")
+                .value_parser(clap::value_parser!(String))
+                .required(false),
         )
-        .get_matches();
+        .get_matches()
 }
 
 fn get_image(path: &str) -> Result<DynamicImage, ImageError> {
@@ -74,12 +79,13 @@ fn get_image(path: &str) -> Result<DynamicImage, ImageError> {
 }
 
 fn get_image_dimensions(image: DynamicImage) -> (u32, u32) {
-    return image.dimensions();
+    image.dimensions()
 }
 
 fn get_filename(path: &str) -> Option<String> {
     let path = Path::new(path);
-    path.file_stem().map(|os_str| os_str.to_string_lossy().to_string())
+    path.file_stem()
+        .map(|os_str| os_str.to_string_lossy().to_string())
 }
 
 fn replace_extension(path: &str, new_extension: &str) -> String {
